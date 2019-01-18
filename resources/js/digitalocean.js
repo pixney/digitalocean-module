17fd560ed23775f8cfe8bbf189358dc16b2141bf
William <william@pixney.com>
2019-01-18 20:47:39 +0100

    First

--- resources/js/digitalocean.js
+++ resources/js/digitalocean.js
@@ -6,8 +6,6 @@ import Droplets from './components/Droplets.vue';
 Vue.component('droplets', Droplets);
 
 var digitaloceana = new Vue({
-    el: '#digitalocean',
-    data: {
-        message: 'Hellos Vue!'
-    }
+    el: '#digitalocean'
+    
 })
